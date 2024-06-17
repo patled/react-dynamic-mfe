bec065351e470bbec0df6bdebee3c6e352f759b0
Patrick Lederer <Patrick.Lederer@brz.eu>
2024-06-17 17:16:51 +0200

    chore: Refactor App component to use CSS modules

--- producer-b/src/App.tsx
+++ producer-b/src/App.tsx
@@ -1,11 +1,7 @@
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
-  return (
-    <>
-      <h1 className="lime">This is Producer B</h1>
-    </>
-  );
+  return <h1 className={styles.main}>This is Producer B</h1>;
 }
 
 export default App;
