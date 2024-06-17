fc675323ca2942f9c7ad1c6524eff4edea4ca460
Patrick Lederer <Patrick.Lederer@brz.eu>
2024-06-17 17:38:39 +0200

    refactor: Update module name in App.tsx for better clarity

--- producer-a/src/App.tsx
+++ producer-a/src/App.tsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 
 function App() {
-  return <h1 className={styles.main}>This is Producer A</h1>;
+  return <h1 className={styles.main}>This is Module A from Producer A</h1>;
 }
 
 export default App;
