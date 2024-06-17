dfca186d46ace004696ee161ef6d40220d30529f
Patrick Lederer <Patrick.Lederer@brz.eu>
2024-06-17 17:16:45 +0200

    style: update class name in App.tsx for better readability

--- producer-a/src/App.tsx
+++ producer-a/src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import styles from "./App.module.css";
 
 function App() {
-  return <h1>This is Producer A</h1>;
+  return <h1 className={styles.main}>This is Producer A</h1>;
 }
 
 export default App;
