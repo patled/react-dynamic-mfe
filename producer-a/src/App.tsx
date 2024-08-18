20bc9682bd43f3f9c618edfa34474841ceb100a6
patled <github@lederer.cloud>
2024-08-18 15:12:38 +0200

    refactor: Improve code clarity and add event dispatch in App.tsx

--- producer-a/src/App.tsx
+++ producer-a/src/App.tsx
@@ -1,7 +1,24 @@
+import React from "react";
 import styles from "./App.module.css";
 
 function App() {
-  return <h1 className={styles.main}>This is Module A from Producer A</h1>;
+  function dispatchChangeEvent(e: React.MouseEvent<HTMLButtonElement>) {
+    console.log("dispatchChangeEvent: ", e);
+    const changeEvent = new CustomEvent("custom-change", {
+      detail: {
+        message: "Hello from Module A",
+      },
+      bubbles: true,
+    });
+    e.currentTarget.dispatchEvent(changeEvent);
+  }
+
+  return (
+    <>
+      <h1 className={styles.main}>This is Module A from Producer A</h1>
+      <button onClick={(e) => dispatchChangeEvent(e)}>Click me</button>
+    </>
+  );
 }
 
 export default App;
